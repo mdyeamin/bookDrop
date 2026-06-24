83b87951187ed62ca65132edbaa421720ef2c60f
mdyeamin <ahmedyeamin05@gmail.com>
2026-06-25 00:43:59 +0600

    server fetch api secure with token

--- src/lib/api/users.js
+++ src/lib/api/users.js
@@ -1,21 +1,22 @@
-import { serverFetch } from "../core/server";
+import { headers } from "next/headers";
+import { auth } from "../auth";
+import { secureServerFetch, serverFetch } from "../core/server";
 
 // get all user from the server
 export const getUsers = async () => {
   // const { data: token } = await authClient.token();
   //   console.log(token?.token);
 
-  // const token = await auth?.api?.getAccessToken({
-  //   headers: await headers(),
-  // });
+  const  tokenData  = await auth?.api?.getToken({
+    headers: await headers(),
+  });
+  const token = tokenData?.token
+  console.log(token);
 
-  return serverFetch("/api/users"); // ← .token
+  return secureServerFetch("/api/users",token); // ← .token
 };
 
-
-
-
-// manage books by admin 
+// manage books by admin
 export const getAllBooksByAdmin = async () => {
   return serverFetch("/api/books");
 };
